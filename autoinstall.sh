f60bdb2fdf3a4d5c88390af2d89edb01b79d4888
Jansen Price <sumpygump@gmail.com>
2012-06-22 00:27:12 -0500

    - Added autoinstall script

---
+++ autoinstall.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+# This script will clone the dotfiles from github and then symlink them to
+# your home directory.
+INSTALL_TO=~
+
+warn() {
+    echo "$1" >&2
+}
+
+die() {
+    warn "$1"
+    exit 1
+}
+
+if [ -e "$INSTALL_TO/dotfiles" ]; then
+    echo -n "$INSTALL_TO/dotfiles already exists. Do you want to continue with symlinking files? (Y/n) "
+    read answer
+    if [ "$answer" == "n" ]; then
+        die "Exiting."
+    fi
+else
+    cd "$INSTALL_TO"
+    git clone git://github.com/sumpygump/dotfiles.git
+fi
+
+symlink_with_checks() {
+    targetname="$1"
+    sourcepath="$2"
+    echo
+    echo "Symlinking $targetname..."
+    cd ~
+    if [ -e "$targetname" ]; then
+        warn "$targetname file already exists."
+        diff -u "$sourcepath" $targetname > /dev/null
+        if [ $? -eq 0 ]; then
+            echo "It looks like the files are identical. Skipping."
+        else
+            echo -n "The files are not the same. Wanna see a diff? (y/N) "
+            read answer
+            if [ "$answer" == "y" ]; then
+                diff -u "$sourcepath" $targetname | less
+                echo -n "Wanna delete this file and then use the symlink? (y/N) "
+                read answer
+                if [ "$answer" == "y" ]; then
+                    rm $targetname
+                    ln -v -s "$sourcepath"
+                fi
+            fi
+        fi
+    else
+        ln -v -s "$sourcepath"
+    fi
+}
+
+symlink_with_checks ".screenrc" "$INSTALL_TO/dotfiles/screen/.screenrc"
+symlink_with_checks ".bash_profile" "$INSTALL_TO/dotfiles/bash/.bash_profile"
+symlink_with_checks ".bashrc" "$INSTALL_TO/dotfiles/bash/.bashrc"
+symlink_with_checks ".bash_aliases" "$INSTALL_TO/dotfiles/bash/.bash_aliases"
+symlink_with_checks ".gitconfig" "$INSTALL_TO/dotfiles/git/.gitconfig"
+
+echo "Done."
